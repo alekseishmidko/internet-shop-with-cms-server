58ce91053a087264fa013ce9de7ed6f67e9ccb0b
alekseishmidko <118507792+alekseishmidko@users.noreply.github.com>
2024-07-29 19:48:28 +0300

    init

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common'
+import { PrismaService } from './prisma.service'
+import { ConfigModule } from '@nestjs/config'
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+@Module({
+	imports: [ConfigModule.forRoot(), AuthModule, UserModule],
+	controllers: [],
+	providers: [PrismaService],
+	exports: [],
+})
+export class AppModule {}
